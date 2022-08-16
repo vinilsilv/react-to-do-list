60b811629adfb8bc8488cdcf39e14f74e49f66e6
Vincius Silva <vinilrsilv@gmail.com>
2022-08-15 22:28:22 -0300

    Different imput when editing

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
 
-  const inputRef = useRef(null)
+  const inputRef = useRef(null);
 
   useEffect(() => {
-    inputRef.current.focus()
-  })
+    inputRef.current.focus();
+  });
 
   const handleChange = (e) => {
     setInput(e.target.value);
@@ -17,27 +17,42 @@ function TodoForm(props) {
     e.preventDefault();
 
     props.onSubmit({
-        id: Math.floor(Math.random() * 10000),
-        text: input
-    })
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
 
     setInput("");
   };
 
   return (
-    <form className="todo-form" 
-    onSubmit={handleSubmit}
-    >
-      <input
-        type="text"
-        placeholder="Add a todo"
-        value={input}
-        name="text"
-        className="todo-input"
-        onChange={handleChange}
-        ref={inputRef}
-      ></input>
-      <button className="todo-button">Add button</button>
+    <form className="todo-form" onSubmit={handleSubmit}>
+      {props.edit ? (
+        <>
+          <input
+            type="text"
+            placeholder="Update your idem"
+            value={input}
+            name="text"
+            className="todo-input edit"
+            onChange={handleChange}
+            ref={inputRef}
+          ></input>
+          <button className="todo-button edit">Update</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="Add a todo"
+            value={input}
+            name="text"
+            className="todo-input"
+            onChange={handleChange}
+            ref={inputRef}
+          ></input>
+          <button className="todo-button">Add button</button>
+        </>
+      )}
     </form>
   );
 }
